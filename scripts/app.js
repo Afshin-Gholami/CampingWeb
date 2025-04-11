95ce7b1a1d2ecda5f3fe86c3f60425e24c355056
viano.ir <gholamiafshin070@gmail.com>
2025-04-12 01:51:47 +0330

    Building the bodies of places and products.

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,27 @@
-const $ =document
-const navBtn = $.querySelector('.nav__btn')
-const navMenu = $.querySelector('.nav-menu')
+const $ = document;
+const navBtn = $.querySelector(".nav__btn");
+const navMenu = $.querySelector(".nav-menu");
+const placeLike = $.querySelector(".place__like");
+const placeLikeIcon = $.querySelectorAll(".place__like-icon");
 
-navBtn.addEventListener('click', menuHandeler)
-navMenu.addEventListener('click',menuHandeler)
+let isLike = false;
 
+navBtn.addEventListener("click", menuHandeler);
+navMenu.addEventListener("click", menuHandeler);
+placeLikeIcon.forEach((likeBtn) =>
+  likeBtn.addEventListener("click", () => {
 
-function menuHandeler(){
-    navBtn.classList.toggle('nav__btn--open')
-    navMenu.classList.toggle('nav-menu--open')
+    if (isLike) {
+      likeBtn.setAttribute("src", "images/svg-icons/heart.svg");
+      isLike = false;
+    } else {
+      likeBtn.setAttribute("src", "images/svg-icons/heart-red.svg");
+      isLike = true;
+    }
+  })
+);
 
+function menuHandeler() {
+  navBtn.classList.toggle("nav__btn--open");
+  navMenu.classList.toggle("nav-menu--open");
 }
