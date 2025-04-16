c84d524fb3fe32656ca799f767a67fc3d09a4618
viano.ir <gholamiafshin070@gmail.com>
2025-04-16 22:34:51 +0330

    fix bog

--- scripts/app.js
+++ scripts/app.js
@@ -2,23 +2,33 @@ const $ = document;
 const navBtn = $.querySelector(".nav__btn");
 const navMenu = $.querySelector(".nav-menu");
 const placeLikeIcon = $.querySelectorAll(".place__like-icon");
-
+const placeLike = $.querySelectorAll(".place__like");
 let isLike = false;
 
-navBtn.addEventListener("click", menuHandeler);
-navMenu.addEventListener("click", menuHandeler);
-placeLikeIcon.forEach((likeBtn) =>
-  likeBtn.addEventListener("click", () => {
-    if (isLike) {
-      likeBtn.src = "images/svg-icons/heart.svg";
-    } else {
-      likeBtn.src = "images/svg-icons/heart-red.svg";
-    }
-    isLike = !isLike;
-  })
-);
-
-function menuHandeler() {
+let menuHandeler =() => {
   navBtn.classList.toggle("nav__btn--open");
   navMenu.classList.toggle("nav-menu--open");
 }
+
+navBtn.addEventListener("click", menuHandeler);
+navMenu.addEventListener("click", menuHandeler);
+
+
+placeLike.forEach((post) => {
+  post.addEventListener("click", () => {
+    if (isLike) {
+      post.innerHTML = `<img
+      src="images/svg-icons/heart-red.svg"
+      alt="Like"
+      class="place__like-icon"
+    />`;
+    } else {
+      post.innerHTML = `<img
+                    src="images/svg-icons/heart.svg"
+                    alt="Like"
+                    class="place__like-icon"
+                  />`;
+    }
+    isLike = !isLike;
+  });
+});
